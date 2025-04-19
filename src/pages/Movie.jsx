b2df7e11f032a4238e7b806cec666038e50bf949
Esther-Sonia <esthersonia21@gmail.com>
2025-04-19 22:54:49 +0300

    Add Movie page to display detailed movie info by ID

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,14 +1,37 @@
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import NavBar from "../components/NavBar";
+
 
 function Movie() {
+  const { id } = useParams();
+  const [movie, setMovie] = useState(null);
+
+  useEffect(() => {
+    fetch(`http://localhost:3000/movies/${id}`)
+      .then((response) => response.json())
+      .then((data) => setMovie(data));
+  }, [id]);
+
+  if (!movie) return <p>Loading...</p>;
+
+
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
-      </header>
+      <NavBar />
+            </header>
       <main>
-        {/* Movie info here! */}
-      </main>
+      <h1>{movie.title}</h1>
+        <p>{movie.time} minutes</p>
+        <div>
+          Genres:{" "}
+          {movie.genres.map((genre, index) => (
+            <span key={index}>{genre}</span>
+          ))}
+        </div>
+              </main>
     </>
   );
 };
