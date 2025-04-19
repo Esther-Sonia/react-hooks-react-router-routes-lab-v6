c139c246162a87126addafe619ca635bb7545111
Esther-Sonia <esthersonia21@gmail.com>
2025-04-19 22:55:57 +0300

    Set up basic routing using React Router in routes.js

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,33 @@
+import Home from "./pages/Home";
+import Movie from "./pages/Movie";
+import Actors from "./pages/Actors";
+import Directors from "./pages/Directors";
+import ErrorPage from "./pages/ErrorPage"; 
+
 
 
 const routes = [
-   
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/directors",
+    element: <Directors />,
+  },
+  {
+    path: "/actors",
+    element: <Actors />,
+  },
+  {
+    path: "/movie/:id",
+    element: <Movie />,
+  },
+  {
+    path: "*",
+    element: <ErrorPage />,
+  },
+
   ];
 
 export default routes;
