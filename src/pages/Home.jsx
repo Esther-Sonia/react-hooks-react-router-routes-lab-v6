c786fc39aaa3f93e6f21c3477a04695222822318
Esther-Sonia <esthersonia21@gmail.com>
2025-04-19 22:51:54 +0300

    Create Home page to render list of movies using MovieCard

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,34 @@
 import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar"; 
+import MovieCard from "../components/MovieCard";
 
 function Home() {
-  return (
+  const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+      fetch("http://localhost:3000/movies")
+      .then((response) => response.json())
+      .then((data) => setMovies(data)); 
+    }, []); 
+  
+    return (
+
+
     <>
       <header>
-        {/* What component should go here? */}
+      <NavBar />      
       </header>
       <main>
-        {/* Info goes here! */}
+      <h1>Home Page</h1>
+      <div>
+      {movies.map((movie) => (
+
+      <MovieCard key={movie.id} title={movie.title} id={movie.id} />
+          ))}
+        </div>
       </main>
     </>
   );
-};
+}
 
 export default Home;
