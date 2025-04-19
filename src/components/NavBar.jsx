213479b87d681c777e18a365b048488dd62962c5
Esther-Sonia <esthersonia21@gmail.com>
2025-04-19 22:58:50 +0300

    Add NavBar component with navigation links to all pages

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,38 @@
 import { NavLink } from "react-router-dom";
 import "./NavBar.css";
 
-function NavBar() {
-  return (
-    <nav className="navbar">
-    
-    </nav>
+  function NavBar() {
+    return (
+      <nav className="navbar">
+        <ul>
+          <li>
+            <NavLink
+              to="/"
+              end
+              className={({ isActive }) => (isActive ? "active" : "")}
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/directors"
+              className={({ isActive }) => (isActive ? "active" : "")}
+            >
+              Directors
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/actors"
+              className={({ isActive }) => (isActive ? "active" : "")}
+            >
+              Actors
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
     );
-};
+  }
 
 export default NavBar;
